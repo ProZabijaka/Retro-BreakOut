d6dbac5a912e68462a57b45cacf2e02156331193
Mateusz Baranowski <bodzio02.60@o2.pl>
2020-02-07 21:59:23 +0100

    Beginning of a project, put in a paddle, a ball, and a background.

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] float minHorizontal = 1f;
+    [SerializeField] float maxHorizontal = 15f;
+    [SerializeField] float ScreenWidthInUnits = 16f;
+    float MousePosInUnits;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MousePosInUnits = (Input.mousePosition.x / Screen.width * ScreenWidthInUnits);
+        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
+        paddlePos.x = Mathf.Clamp(MousePosInUnits,minHorizontal,maxHorizontal);
+        transform.position = paddlePos;
+    }
+}

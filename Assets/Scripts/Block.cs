299a0c4a0cfccde08206c00516586aef14f9d024
Mateusz Baranowski <bodzio02.60@o2.pl>
2020-02-15 02:33:56 +0100

    Added 2nd level, set up blocks in different way, changed background, added sound

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    [SerializeField] AudioClip breakSound;
+
+
+    // Cached reference
+    Level level;
+
+    private void Start()
+    {
+        level = FindObjectOfType<Level>();
+        level.BreakableBlocksAmount();
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        DestroyBlock();
+    }
+
+    private void DestroyBlock()
+    {
+        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
+        Destroy(gameObject, 0);
+        level.BlockDestroyed();
+    }
+}
